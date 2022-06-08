abc96cbf8e1abf0b2d79c74b546a0e06c365ec80
MTDataScience <jaraujo@metropolitan-touring.com>
2022-06-08 12:10:18 -0500

    Update Librerias_Minimas.R

--- Librerias_Minimas.R
+++ Librerias_Minimas.R
@@ -2,8 +2,7 @@
 # LIBRERIAS MINIMAS PARA INSTALACION EN R #
 ###########################################
 
-#En este script estan las librerias minimas que deberian instalarse en R para 
-#el area de data science
+#En este script estan las librerias minimas que deberian instalarse en R para el area de data science
 
 #MANIPULACION DE DATOS
 install.packages('RODBC', dependencies = T)
@@ -14,8 +13,7 @@ install.packages('fuzzyjoin',dependencies = T)
 
 #ANALISIS DE DATOS
 install.packages('caret', dependencies = T)
-install.packages("h2o", type="source", 
-                 repos="https://h2o-release.s3.amazonaws.com/h2o/rel-zumbo/2/R")
+install.packages("h2o", type="source", repos="https://h2o-release.s3.amazonaws.com/h2o/rel-zumbo/2/R")
 install.packages('mclust', dependencies = T)
 install.packages('forecast', dependencies = T)
 install.packages('mlr3', dependencies = T)
